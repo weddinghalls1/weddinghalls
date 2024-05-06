b8e6b7e2e93c1ae5c85fca051d4535d282a2d098
Ranin7 <raninabdullah11@gmail.com>
2024-05-06 10:15:57 +0300

    cnacel merge

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,20 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:untitled/screen/routes/router.dart';
-import 'firebase_options.dart';
 
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(const MainPage());
+void main() {
+  runApp(const MainApp());
 }
 
-class MainPage extends StatefulWidget {
-  const MainPage({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
-  @override
-  State<MainPage> createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-      return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        initialRoute: '/',
-        routes: kRoutes);
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
   }
 }
