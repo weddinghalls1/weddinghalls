f90e86f3b342ed9f5e03682bf72b48399c6e515d
rahafhamayel <116808826+rahafhamayel@users.noreply.github.com>
2024-05-15 11:26:59 +0300

    up22146

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:weddinghalls/routes/router.dart';
 import 'package:weddinghalls/views/descriptionHalls.dart';
 import 'firebase_options.dart';
 
