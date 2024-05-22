dba59994adb89080f614ca588d24216960c9fc22
rahafhamayel <116808826+rahafhamayel@users.noreply.github.com>
2024-05-22 12:40:47 +0300

    update2

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class _MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
       return MaterialApp(
         debugShowCheckedModeBanner: false,
-        home:ReservationPage();
+        home:ReservationPage(),
         );
   }
 }
