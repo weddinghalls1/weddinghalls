002866057cd10f76ca38a8e4a7d844ca27d343ef
rahafhamayel <116808826+rahafhamayel@users.noreply.github.com>
2024-05-28 22:03:03 +0300

    up2

--- lib/model/user_model.dart
+++ lib/model/user_model.dart
@@ -2,18 +2,23 @@ class UserModel {
   final String email;
   final String profileImageUrl;
   final String username;
+  final String token;
 
   UserModel({
     required this.email,
     required this.profileImageUrl,
     required this.username,
+    required this.token,
+
   });
 
   factory UserModel.fromMap(Map<String, dynamic> data) {
     return UserModel(
-      email: data['email'],
-      profileImageUrl: data['profileImageUrl'],
-      username: data['username'],
+      email: data['email'] ??'',
+      profileImageUrl: data['profileImageUrl']??'',
+      username: data['username']??'',
+      token: data['token'] ?? '',
+
     );
   }
 }
--- lib/view_model/user_viewmodel.dart
+++ lib/view_model/user_viewmodel.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-
 import '../model/user_model.dart';
 
 class UserViewModel {
@@ -10,6 +9,8 @@ class UserViewModel {
       DocumentSnapshot doc = await _firestore.collection('profile').doc(token).get();
       if (doc.exists) {
         return UserModel.fromMap(doc.data() as Map<String, dynamic>);
+      } else {
+        print("Document does not exist");
       }
     } catch (e) {
       print("Error getting user data: $e");
