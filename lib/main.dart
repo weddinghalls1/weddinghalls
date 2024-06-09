e3bcd0b9017f45538232bdb62a5eeb9e34b67025
Mohammad Soqy <96316121+Mohamadsoqy1@users.noreply.github.com>
2024-06-09 13:14:53 +0300

    Edit on AddCard to Store on Firestore

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:weddinghalls/views/My_halls.dart';
+import 'package:weddinghalls/views/addNewCard_view.dart';
 import 'package:weddinghalls/views/select_page.dart';
 import 'package:weddinghalls/views/admin_description_view.dart';
 import 'package:weddinghalls/views/combined_view.dart';
@@ -50,7 +51,7 @@ class _MainPageState extends State<MainPage> {
 
 
 
-      home:HallSelectionPage(),
+      home:AddCardPayment(),
       //HomePage(),
 
 
